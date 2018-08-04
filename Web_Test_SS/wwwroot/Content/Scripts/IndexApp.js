612102a517d7425390c6e0adc6d05492fb7c8c34
Volodymyr <voh4avasa@gamil.com>
2018-08-04 19:27:01 +0300

    Develop frontend

---
+++ Web_Test_SS/wwwroot/Content/Scripts/IndexApp.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module('WatchesApp', []);
+
+app.controller("SubscribeCtrl", function ($scope) {
+    $scope.subscribe = function (email,) {
+        
+        alert("Email is " + email);
+        //Написати добавлення емейлу в базу
+    }
+});
+
+app.controller("BodyCtrl", function ($scope) {
+    $scope.dropdown = true;
+    $scope.search = "";
+
+    $scope.Watches = [{ image: "../Content/WatchesImage/HamiltonH76512733.jpg", name: "Hamilton H76512733", price: 16900, text: "Виробник: Швейцарія / механізм: кварцовий / стікло: сапфіровое / тип індикації: стрілковий / корпус: сталь / водонепроникність: 100 м / ремінь: шкіряний / тип: чоловічі" },
+        { image: "../Content/WatchesImage/HamiltonH82305931.jpg", name: "Hamilton H82305931", price: 21850, text: "Виробник: Швейцария / механізм: механічний з автоподзаводом / стікло: сапфіровое / тип індикацхї: стрілковий / корпус: сталь / водонепроникність: 100 м / ремінь: тканинний / тип: чоловічі" },
+        { image: "../Content/WatchesImage/Atlantic50744.41.21.jpg", name: "Atlantic 50744.41.21", price: 15370, text: "Виробник: Швейцарія / тип: чоловічі / механізм: механічний с автоподзаводом / стікло: сапфіровое / тип індикацхї: стрілковий / корпус: сталь / водонепроникність: 50 м / ремінь: шкіряний"}];
+
+    $scope.ShowDropdown = function () {
+        $scope.dropdown = false;
+    }
+
+    $scope.FilterbyName = function (item) {
+        if (item.name.toLowerCase().match($scope.search.toLowerCase()) !== null) {
+            return true;
+        }
+        else { return false; }
+    }
+});

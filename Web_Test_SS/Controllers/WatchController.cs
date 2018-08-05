b3c14b976b9e1a218e36330e36e4688bca03d4f5
Volodymyr <voh4avasa@gamil.com>
2018-08-05 20:50:10 +0300

    Config Web api and set Modal window

---
+++ Web_Test_SS/Controllers/WatchController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web_Test_SS.Model;
+
+namespace Web_Test_SS.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class WatchController : Controller
+    {
+        // GET api/watch
+        [HttpGet]
+        public IEnumerable<Watch> Get()
+        {
+            List<Watch> watches = new List<Watch>();
+
+            Watch watch = new Watch
+            {
+                Image = "../Content/WatchesImage/HamiltonH76512733.jpg",
+                Name = "Hamilton H76512733",
+                Text = "Виробник: Швейцарія / механізм: кварцовий / стікло: сапфіровое / тип індикації: стрілковий / корпус: сталь / водонепроникність: 100 м / ремінь: шкіряний / тип: чоловічі",
+                Price = 16900
+            };
+
+            watches.Add(watch);
+
+            watch = new Watch
+            {
+                Image = "../Content/WatchesImage/HamiltonH82305931.jpg",
+                Name = "Hamilton H82305931",
+                Text = "Виробник: Швейцария / механізм: механічний з автоподзаводом / стікло: сапфіровое / тип індикацхї: стрілковий / корпус: сталь / водонепроникність: 100 м / ремінь: тканинний / тип: чоловічі",
+                Price = 21850
+            };
+
+            watches.Add(watch);
+
+            watch = new Watch
+            {
+                Image = "../Content/WatchesImage/Atlantic50744.41.21.jpg",
+                Name = "Atlantic 50744.41.21",
+                Text = "Виробник: Швейцарія / тип: чоловічі / механізм: механічний с автоподзаводом / стікло: сапфіровое / тип індикацхї: стрілковий / корпус: сталь / водонепроникність: 50 м / ремінь: шкіряний",
+                Price = 15370
+            };
+
+            watches.Add(watch);
+
+            return watches;
+
+        }
+    }
+}
---
+++ Web_Test_SS/Model/Watch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Test_SS.Model
+{
+    public class Watch
+    {
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public int Price { get; set; }
+    }
+}
